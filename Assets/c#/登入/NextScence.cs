de29fd9445e29d297db287c9b968e66f81294785
Yanhungliu <davidalan0826@gmail.com>
2024-01-26 21:20:17 +0800

    1/26 1.改善MainMenu(登入畫面)的程式集和多餘物件 2.新增Perfab 資料夾 放入MainMenu 的背景組件

--- Assets/c#/登入/NextScence.cs
+++ Assets/c#/登入/NextScence.cs
@@ -7,15 +7,17 @@ using UnityEngine.UI;
 
 public class NextScence : MonoBehaviour
 {
+    
+   
+
+    // Update is called once per frame
+     void Update()
+     {
+        Invoke("NextScences", 2f);
+
+     }
 
 
-    public Text mytext;
-    public Image myImage;
-    // Start is called before the first frame update
-    void Start()
-    {
-        InvokeRepeating("showHide", 0.5f, 0.5f);
-    }
 
     void NextScences()
     {
@@ -23,29 +25,5 @@ public class NextScence : MonoBehaviour
         {
             SceneManager.LoadScene(1);
         }
-    }
-
-
-    void showHide()
-    {
-
-        if (mytext.text == "")
-        { //¦pªG mytext¤º®e¬OªÅªº
-
-            mytext.text = "°´æIßMÈëß[‘ò...."; //±N mytext¤º®e§ïÅÜ
-        }
-        else
-        { //§_«h
-
-            mytext.text = ""; //±N mytext¤º®e§ï¦¨ªÅªº
-
-        }
-
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        Invoke("NextScences", 2f);
-
     }
 }
